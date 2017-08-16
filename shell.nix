acc4c65f4dd4debbe1fe936844c8082e0266fc95
Benjamin Smith <polynomial@gmail.com>
2017-08-16 14:52:28 -0700

    update nix-shell and rebar3

--- shell.nix
+++ shell.nix
@@ -1,44 +1,13 @@
-# This allows a user to run nix-shell in the tech repo and then all
-# of their system dependencies will be handled. More info on NixOS:
-#  https://nixos.org/
-#  http://nixos.org/nix/manual/#sec-nix-shell
-
-{ systemNixpkgs ? import <nixpkgs> {}
-}:
-
+{ bootFetchgit ? (import <nixpkgs> {}).fetchgit }:
 let
-  inherit (systemNixpkgs) stdenv fetchzip;
-    version = import ./etc/version.nix;
-    getUrl = name: channels:
-        "${channels.${name}.baseUrl}/${channels.${name}.version}/nixexprs.tar.xz";
-    getSha = name: channels: channels.${name}.sha256;
-    getVer = name: channels: channels.${name}.version;
-    fetchChannel = url: sha256: name:
-        fetchzip { inherit url sha256 name; };
-    channelPath = name: channels: fetchChannel
-        (getUrl name channels)
-        (getSha name channels)
-        "${name}-${getVer name channels}";
-    nixpkgsPath = builtins.toPath (channelPath "nixpkgs" version.channels);
-    nixpkgs = import nixpkgsPath {
-      config.allowUnfree = true;
-    };
-    nixos = import "${nixpkgsPath}/nixos" {};
+  pkgs = import (bootFetchgit (import ./version.nix)) {};
+  inherit (pkgs) stdenv;
 
+  appRoot = builtins.toPath ./.;
+  version = "0.1.0";
 in stdenv.mkDerivation {
-    name = "e-tech";
+  inherit version;
+  name = "dalmatinerdb-${version}";
 
-    buildInputs = with nixpkgs; [
-        # build
-        git
-        gnumake
-        erlangR18
-        linuxHeaders
-    ];
-    shellHook = ''
-        export NIX_PATH="nixpkgs=${nixpkgsPath}"
-        export NIX_PATH="$NIX_PATH:nixos=${nixpkgsPath}/nixos"
-
-    '';
+  builtInputs = with pkgs; [ erlang stdenv.cc ];
 }
-
