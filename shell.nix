85eb8aa35111d1e67c732bb348b5d91870f5d5c9
System administrator <root@ip-10-1-7-197.ec2.internal>
2017-08-17 22:17:37 +0000

    fails with /data/src/dalmatinerdb/_build/default/lib/hdr_histogram/c_src/hdr_histogram_log.c:13:18: fatal error: zlib.h: No such file or directory

--- shell.nix
+++ shell.nix
@@ -1,13 +1,42 @@
-{ bootFetchgit ? (import <nixpkgs> {}).fetchgit }:
+# This allows a user to run nix-shell in the tech repo and then all
+# of their system dependencies will be handled. More info on NixOS:
+#  https://nixos.org/
+#  http://nixos.org/nix/manual/#sec-nix-shell
+
+{ systemNixpkgs ? import <nixpkgs> {}
+}:
+
 let
-  pkgs = import (bootFetchgit (import ./version.nix)) {};
-  inherit (pkgs) stdenv;
+  inherit (systemNixpkgs) stdenv fetchzip;
+    version = import ./etc/version.nix;
+    getUrl = name: channels:
+        "${channels.${name}.baseUrl}/${channels.${name}.version}/nixexprs.tar.xz";
+    getSha = name: channels: channels.${name}.sha256;
+    getVer = name: channels: channels.${name}.version;
+    fetchChannel = url: sha256: name:
+        fetchzip { inherit url sha256 name; };
+    channelPath = name: channels: fetchChannel
+        (getUrl name channels)
+        (getSha name channels)
+        "${name}-${getVer name channels}";
+    nixpkgsPath = builtins.toPath (channelPath "nixpkgs" version.channels);
+    nixpkgs = import nixpkgsPath {
+      config.allowUnfree = true;
+    };
+    nixos = import "${nixpkgsPath}/nixos" {};
 
-  appRoot = builtins.toPath ./.;
-  version = "0.1.0";
 in stdenv.mkDerivation {
-  inherit version;
-  name = "dalmatinerdb-${version}";
+    name = "dalmatinerdb";
 
-  builtInputs = with pkgs; [ erlang stdenv.cc ];
+    buildInputs = with nixpkgs; [
+        # build
+        git
+        erlangR18
+    ];
+    shellHook = ''
+        export NIX_PATH="nixpkgs=${nixpkgsPath}"
+        export NIX_PATH="$NIX_PATH:nixos=${nixpkgsPath}/nixos"
+
+    '';
 }
+
